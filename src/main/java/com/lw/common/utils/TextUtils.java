1188c097594c850b397160813d83fd9a9d23e365
lw7068 <lw7068@HKGY170121.hikyjy.com.cn>
2017-07-25 10:38:17 +0800

    添加js

--- src/main/java/com/lw/common/utils/TextUtils.java
+++ src/main/java/com/lw/common/utils/TextUtils.java
@@ -1,4 +1,7 @@
 package com.lw.common.utils;
+
+import java.lang.reflect.Proxy;
+
 public class TextUtils {
   public static String camelToUnderline(final String input) {
     final StringBuilder output = new StringBuilder();
@@ -6,6 +9,7 @@ public class TextUtils {
     for (int index = 0; index < length; index++) {
       final char charAt = input.charAt(index);
       if (Character.isUpperCase(charAt)) {
+        Proxy proxy;
         output.append('_');
         output.append(Character.toLowerCase(charAt));
       } else {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <body>
-<h2>Hello World!</h2>
+  <h2>Hello World!</h2>
+  <div class="div1">测试</div>
 </body>
 </html>
